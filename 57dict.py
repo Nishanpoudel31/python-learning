2e085d18d83b5bc16dc9ee0fd91c8688a300edd7
Nishan Poudel <poudelnishan2055.com>
2025-01-24 20:22:56 +0545

    .

---
+++ 57dict.py
@@ -0,0 +1,29 @@
+items_price = {
+    "SamsungM11":"500",
+    "SamsungM13":"750",
+    "SamsungM15":"800",
+    "SamsungS2511":"1000",
+    "SamsungA25":"45000"
+    
+}
+for k,v in items_price.items():
+    print(f"The price of {k} is {v}.")
+
+for v in items_price.values():#This prints values only
+    print(v)
+
+vegetables={
+    "Cauliflower":"150",
+    "Potato":"80",
+    "Carrot":"75",
+    "Radish":"125",
+    "Saag":"220",
+    "Tomato":"42",
+    "Gourd":"187",
+    "Pumpkin":"147"
+}
+for k,v in vegetables.items():
+    print(f"The price of {k} is {v}.")
+
+for k in vegetables.keys():#tHISS PRINT keys only.
+    print(k)
